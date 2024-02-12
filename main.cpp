46fbe7f6732287725da20da3a5fdc5c5d78ff62f
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2024-02-12 16:35:14 +0000

    - Finished Q, added right and centre align

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main() {
     vector<string> lines;
     string userInput;
     int maxWordLength = 0;
+    string alignOption = "0";
 
     cout << "Enter your text (use '\\' on a line by itself to end):\n";
 
@@ -29,14 +30,55 @@ int main() {
     //change border length
     string border = string(maxWordLength + 4, '*');
 
+    cout << "Choose text align:\n[0] - Left\n[1] - Right\n[2] - Centre\nEnter:" << endl;
+    getline(cin, alignOption);
+
     cout << border << endl; //top border
 
-    //print each line with side borders
-    for (const string& line : lines) {
-        cout << "* " << line;
-        //go to end of line with char if it doesn't fit
-        cout << string(maxWordLength - line.length(), ' ');
-        cout << " *" << endl;
+    //middle parts of message
+    switch (stoi(alignOption)) {
+        case 0:
+            //left align (normal logic)
+
+            //print each line with side borders
+            for (const string& line : lines) {
+                cout << "* " << line;
+                //go to end of line with char if it doesn't fit
+                cout << string(maxWordLength - line.length(), ' ');
+                cout << " *" << endl;
+            }
+
+            break;
+
+        case 1:
+            //right align (normal logic but from right)
+            for (const string& line : lines) {
+                //start line with all the spaces
+                cout << "* " << string(maxWordLength - line.length(), ' ');
+                cout << line << " *" << endl;
+            }
+
+            break;
+
+        case 2:
+            //centre align (add equal to both sides / +1 to right)
+            for (const string& line : lines) {
+                //if equal to maxWordLength, don't change these!
+                int leftPad = 0;
+                int rightPad = 0;
+
+                if (line.length() != maxWordLength) {
+                    leftPad = (maxWordLength - line.length()) / 2;
+                    rightPad = (maxWordLength - line.length()) - leftPad;
+                }
+
+                cout << "* " << string(leftPad, ' ') << line;
+                //go to end of line with char if it doesn't fit
+                cout << string(rightPad, ' ');
+                cout << " *" << endl;
+            }
+
+            break;
     }
 
     cout << border << endl; //bottom border
